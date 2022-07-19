ce93a3b974bc574bb95f181a488c8d6da1581196
Andrii Popov <popov.andrey80@gmail.com>
2022-07-19 20:22:14 +0300

    fix mix up files

--- js/task.js
+++ js/task.js
@@ -1,11 +1,11 @@
-const banksList = document.querySelector(".banks");
-const banksInformationEl = document.querySelector(".bank-information");
+const banksList = document.querySelector('.banks');
+const banksInformationEl = document.querySelector('.bank-information');
 // console.log(banksList);
 
 const banks = [
   {
     id: 1,
-    name: "Mono",
+    name: 'Mono',
     interestRate: 5,
     maxLoan: 500000,
     minPayment: 1000,
@@ -13,7 +13,7 @@ const banks = [
   },
   {
     id: 2,
-    name: "Privat",
+    name: 'Privat',
     interestRate: 7,
     maxLoan: 1000000,
     minPayment: 5000,
@@ -33,23 +33,14 @@ function createMarkupBank({ name, id }) {
     </li>`;
 }
 function createBankList(banks, callback) {
-  return banks.map((bank) => callback(bank)).join("");
+  return banks.map(bank => callback(bank)).join('');
 }
 
 // console.log(createBankList(banks, createMarkupBank));
 
-banksList.insertAdjacentHTML(
-  "beforeend",
-  createBankList(banks, createMarkupBank)
-);
+banksList.insertAdjacentHTML('beforeend', createBankList(banks, createMarkupBank));
 
-function createMarkupBankInformation({
-  name,
-  interestRate,
-  maxLoan,
-  minPayment,
-  loanTerm,
-}) {
+function createMarkupBankInformation({ name, interestRate, maxLoan, minPayment, loanTerm }) {
   return `<li>Bank: ${name}</li>
     <li>Mortgage Size, $: ${maxLoan}</li>
     <li>Minimum down payment, $: ${minPayment}</li>
@@ -57,12 +48,25 @@ function createMarkupBankInformation({
     <li>Interest rate, %: ${interestRate}</li>`;
 }
 
-banksList.addEventListener("click", (event) => {
+banksList.addEventListener('click', event => {
   const bankId = event.target.dataset.id;
 
-  const bank = banks.find((bank) => {
+  const bank = banks.find(bank => {
     return Number(bankId) === bank.id;
   });
 
   banksInformationEl.innerHTML = createMarkupBankInformation(bank);
 });
+
+//const openBtn = document.querySelector(".open");
+const overlay = document.querySelector('.overlay');
+//const closeBtn = document.querySelector(".close");
+const startBtn = document.querySelector('.start');
+
+function toggleModal() {
+  overlay.classList.toggle('hide');
+}
+
+startBtn.addEventListener('click', toggleModal);
+//closeBtn.addEventListener("click", toggleModal);
+//openBtn.addEventListener("click", toggleModal);

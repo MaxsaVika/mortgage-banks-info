452b8b8793c0d206c585eb3320c21ebe8c0f4b0d
MaxsaVika <viktoria.plotitsyna@gmail.com>
2022-07-20 17:08:04 +0300

    Add btn-edit&del

--- js/task.js
+++ js/task.js
@@ -1,11 +1,11 @@
-const banksList = document.querySelector('.banks');
-const banksInformationEl = document.querySelector('.bank-information');
+const banksList = document.querySelector(".banks");
+const banksInformationEl = document.querySelector(".bank-information");
 // console.log(banksList);
 
 const banks = [
   {
     id: 1,
-    name: 'Mono',
+    name: "Mono",
     interestRate: 5,
     maxLoan: 500000,
     minPayment: 1000,
@@ -13,7 +13,7 @@ const banks = [
   },
   {
     id: 2,
-    name: 'Privat',
+    name: "Privat",
     interestRate: 7,
     maxLoan: 1000000,
     minPayment: 5000,
@@ -24,23 +24,37 @@ const banks = [
 function createMarkupBank({ name, id }) {
   return `<li class="banks__item" data-id='${id}'>
         <p>${name}</p>
-      <button class="banks__edit--button" type="button">
-        Edit
+      <button class="banks__edit--button banks-btn" type="button">
+      <svg class="form-btn-icon" width="24" height="24">
+      <use href="./img/symbol-defs.svg#icon-edit"></use>
+  </svg>
       </button>
-      <button class="banks__remove--button" type="button">
-        x
+      
+      <button class="banks__remove--button banks-btn" type="button">
+      <svg class="form-btn-icon" width="24" height="24">
+      <use href="./img/symbol-defs.svg#icon-x"></use>
+  </svg>
       </button>
     </li>`;
 }
 function createBankList(banks, callback) {
-  return banks.map(bank => callback(bank)).join('');
+  return banks.map((bank) => callback(bank)).join("");
 }
 
 // console.log(createBankList(banks, createMarkupBank));
 
-banksList.insertAdjacentHTML('beforeend', createBankList(banks, createMarkupBank));
+banksList.insertAdjacentHTML(
+  "beforeend",
+  createBankList(banks, createMarkupBank)
+);
 
-function createMarkupBankInformation({ name, interestRate, maxLoan, minPayment, loanTerm }) {
+function createMarkupBankInformation({
+  name,
+  interestRate,
+  maxLoan,
+  minPayment,
+  loanTerm,
+}) {
   return `<li>Bank: ${name}</li>
     <li>Mortgage Size, $: ${maxLoan}</li>
     <li>Minimum down payment, $: ${minPayment}</li>
@@ -48,10 +62,10 @@ function createMarkupBankInformation({ name, interestRate, maxLoan, minPayment, 
     <li>Interest rate, %: ${interestRate}</li>`;
 }
 
-banksList.addEventListener('click', event => {
+banksList.addEventListener("click", (event) => {
   const bankId = event.target.dataset.id;
 
-  const bank = banks.find(bank => {
+  const bank = banks.find((bank) => {
     return Number(bankId) === bank.id;
   });
 
@@ -59,14 +73,14 @@ banksList.addEventListener('click', event => {
 });
 
 //const openBtn = document.querySelector(".open");
-const overlay = document.querySelector('.overlay');
+const overlay = document.querySelector(".overlay");
 //const closeBtn = document.querySelector(".close");
-const startBtn = document.querySelector('.start');
+const startBtn = document.querySelector(".start");
 
 function toggleModal() {
-  overlay.classList.toggle('hide');
+  overlay.classList.toggle("hide");
 }
 
-startBtn.addEventListener('click', toggleModal);
+startBtn.addEventListener("click", toggleModal);
 //closeBtn.addEventListener("click", toggleModal);
 //openBtn.addEventListener("click", toggleModal);
